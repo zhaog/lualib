172e2d709975b9572058fa62dc58c1be5e88ffd6
zhaoguang <425044891@qq.com>
2014-10-25 18:24:26 +0800

    Lua类库

---
+++ LuaLibTest/LuaTest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<windows.h>
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+#include "luaconf.h"
+
+
+int main(int argc, char* argv[])
+{
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	luaL_dofile(L, "./test.lua");
+	lua_close(L);
+	system("pause");
+	return 0;
+}
---
+++ LuaLibTest/test.lua
@@ -0,0 +1,10 @@
+function show()
+	local b = {}
+	local index
+	
+	for index = 1,10,1 do
+		print(index)
+	end
+end
+
+show()
