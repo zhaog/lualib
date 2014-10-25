fb180bb6c3d75d96b1687380ef147e16e9ab1bb3
zhaoguang <425044891@qq.com>
2014-10-25 21:40:40 +0800

    update LuaTest.c

--- LuaLibTest/LuaTest.c
+++ LuaLibTest/LuaTest.c
@@ -8,12 +8,14 @@ Test the Lua lib
 #include "lauxlib.h"
 #include "luaconf.h"
 
-
+/**
+WHY
+**/
 int main(int argc, char* argv[]){
-
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
-	luaL_dofile(L, "./test.lua");
+	luaL_dofile(L,"./test.lua");
+
 	lua_close(L);
 	system("pause");
 	return 0;
